0feee4a326189356d1946682e62544202f3c8157
KOLM Benjamin <126233198+KOLMBenjamin@users.noreply.github.com>
2023-10-10 10:04:20 +0200

    Add files via upload

---
+++ KOLM-Benjamin/3.10 swp HÜ {.js
@@ -0,0 +1,14 @@
+function f() {
+  const u = 0;
+  while(u <20){
+      let zufallszahl = Math.round(Math.random() *20 +1);
+      if(zufallszahl > 100){
+          throw new Error ("Fehler!");
+          return fertigstellung;
+      }
+      u++;
+  }
+}
+try {error}
+
+catch (error) {console.log(error)}
